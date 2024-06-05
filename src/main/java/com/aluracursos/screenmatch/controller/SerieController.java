514b3c6ea987780549485f8553ed287d082ab8f6
luys <sanchezluys@gmail.com>
2024-06-05 12:18:31 -0500

    clase 02-02 se cambia la logica del controlador serie para que envie una lista al front, se presentan errores en la libreria jackson.databind

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,12 +1,22 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    // agregando las anotaciones para conectarse al repositorio
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "Este es mi primer mensaje en la website";
+
+    public List<Serie> obtenerTodasLasSeries(){
+        return repositorio.findAll();
     }
 }
